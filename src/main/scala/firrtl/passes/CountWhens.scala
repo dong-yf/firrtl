aea32b3c3abf0db2624dbaa8a94b857354400f74
dong-yf <2959032105@qq.com>
2024-01-22 19:12:33 +0800

    add annotation for Conditionally

--- src/main/scala/firrtl/passes/CountWhens.scala
+++ src/main/scala/firrtl/passes/CountWhens.scala
@@ -7,7 +7,18 @@ import firrtl.annotations._
 import firrtl.Mappers._
 import firrtl.Utils._
 import firrtl.options.Dependency
-import java.util.concurrent.locks.Condition
+
+import firrtl.transforms._
+import scala.collection.mutable
+
+
+case class ConditionalEdgeAnnotation(parentCondition: Target, childCondition: Target)
+    extends MultiTargetAnnotation {
+
+  override val targets: Seq[Seq[Target]] = Seq(Seq(parentCondition), Seq(childCondition))
+  override def duplicate(n: Seq[Seq[Target]]): ConditionalEdgeAnnotation = this.copy(n(0).head, n(1).head)
+}
+
 
 object CountWhens extends Transform with DependencyAPIMigration {
   override def prerequisites =
@@ -20,25 +31,42 @@ object CountWhens extends Transform with DependencyAPIMigration {
 
   override def invalidates(a: Transform) = false
 
-  def onModule(m: DefModule): DefModule = {
+  def onModule(m: DefModule, main: String): (DefModule, Seq[Annotation]) = {
     println(m.name)
-    m.map(onStmt)
+    val annos = mutable.ListBuffer[Annotation]()
+    m.map(x => onStmt(x, main, m.name, None, annos))
+    val newAnnos = annos.toList
+    (m, newAnnos)
   }
 
-  def onStmt(s: Statement): Statement = {
-    s.map(onStmt) match {
-      case c: Conditionally =>
-        println(s"Conditionally: ${c.pred}")
-        c
-      case x => x
-    }
+  def onStmt(s: Statement, main: String, m: String, parent: Option[ir.Expression], annos: mutable.ListBuffer[Annotation]): Statement = s match {
+    case c: Conditionally =>
+      println(s"Conditionally: ${c.pred}")
+      parent match {
+        case Some(p) => 
+          val parentTarget = Utils.toTarget(main, m)(p)
+          val childTarget = Utils.toTarget(main, m)(c.pred)
+          val edgeAnno = ConditionalEdgeAnnotation(parentTarget, childTarget)
+          val dontTouchParent = DontTouchAnnotation(parentTarget)
+          val dontTouchChild = DontTouchAnnotation(childTarget)
+          annos.append(edgeAnno)
+          annos.append(dontTouchParent)
+          annos.append(dontTouchChild)
+        case None =>
+      }
+      println(s"edge: ${parent} --> ${c.pred}")
+      onStmt(c.conseq, main, m, Some(c.pred), annos)
+      onStmt(c.alt, main, m, Some(c.pred), annos)
+      c
+    case x => x.map(onStmt(_, main, m, parent, annos))
   }
 
   def execute(state: CircuitState): CircuitState = {
     val circuit = state.circuit
     
-    circuit.map(onModule(_))
+    val modsAndAnnos = circuit.modules.map(onModule(_, circuit.main))
+    val newAnnos = modsAndAnnos.flatMap(_._2)
   
-    state
+    state.copy(annotations = newAnnos ++: state.annotations)
   }
 }
